98c465c11f8a4002b8b819cc9a5e82308eb6134b
나똥 <129125532+hyeon0902@users.noreply.github.com>
2023-11-30 12:36:31 +0900

    daw

--- vue/vue-test/src/App.vue
+++ vue/vue-test/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <VueComponent></VueComponent>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <script>
-import VueComponent from './components/VueComponent.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld
-    VueComponent
+    HelloWorld
   }
 }
 </script>
@@ -21,6 +24,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
