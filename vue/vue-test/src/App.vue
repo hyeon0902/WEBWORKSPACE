1d13e5b99e8ad9e88fb058c2ca375ae996f1aeb7
나똥 <129125532+hyeon0902@users.noreply.github.com>
2023-11-30 12:29:29 +0900

    fdaw

--- vue/vue-test/src/App.vue
+++ vue/vue-test/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <VueComponent></VueComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueComponent from './components/VueComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    VueComponent
   }
 }
 </script>
---
+++ vue/vue-test/src/components/VueComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <label><input type="text" v-model="msg" /></label>
+      <p>{{ msg }}</p>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'VueComponent',
+    data() {
+      return {
+        msg: '2023-11-30',
+      };
+    },
+  };
+  </script>
+  <style>
+  p {
+    background-color: aquamarine;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
